2b4619773dbc2f63bfc188b66a8568d29dc51fa8
Manh-Hiep Dao <1907.hiepmanhdao@gmail.com>
2023-12-10 09:12:59 +0700

    Update version 23.12.10: Fetch to the latest gfmpw-1d; Modify the size of array LA Probes

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -35,122 +35,187 @@
  *-------------------------------------------------------------
  */
 
-module user_proj_example #(
-    parameter BITS = 16
-)(
+module user_proj_example (
 `ifdef USE_POWER_PINS
-    inout vccd1,	// User area 1 1.8V supply
-    inout vssd1,	// User area 1 digital ground
+    inout vdd,	// User area 1 1.8V supply
+    inout vss,	// User area 1 digital ground
 `endif
 
     // Wishbone Slave ports (WB MI A)
     input wb_clk_i,
     input wb_rst_i,
-    input wbs_stb_i,
-    input wbs_cyc_i,
-    input wbs_we_i,
-    input [3:0] wbs_sel_i,
-    input [31:0] wbs_dat_i,
-    input [31:0] wbs_adr_i,
-    output wbs_ack_o,
-    output [31:0] wbs_dat_o,
-
+  
     // Logic Analyzer Signals
-    input  [127:0] la_data_in,
-    output [127:0] la_data_out,
-    input  [127:0] la_oenb,
+    input  [63:0] la_data_in,
+    output [63:0] la_data_out,
+    input  [63:0] la_oenb
 
     // IOs
-    input  [BITS-1:0] io_in,
-    output [BITS-1:0] io_out,
-    output [BITS-1:0] io_oeb,
-
-    // IRQ
-    output [2:0] irq
+    // input io_in,
+    // output [2:0] io_out
 );
-    wire clk;
-    wire rst;
+    reg [2:0] reg_num1, reg_num2;
 
-    wire [BITS-1:0] rdata; 
-    wire [BITS-1:0] wdata;
-    wire [BITS-1:0] count;
-
-    wire valid;
-    wire [3:0] wstrb;
-    wire [BITS-1:0] la_write;
-
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = {{(32-BITS){1'b0}}, rdata};
-    assign wdata = wbs_dat_i[BITS-1:0];
-
-    // IO
-    assign io_out = count;
-    assign io_oeb = {(BITS){rst}};
-
-    // IRQ
-    assign irq = 3'b000;	// Unused
-
+    //wire [BITS-1:0] count;
+    wire [2:0] sum, status;
+    wire done;
+    //wire done;
+    wire wena;
+    //wire valid;
+    // wire [3:0] wstrb;
+    wire [31:0] la_write;
     // LA
-    assign la_data_out = {{(128-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:64-BITS] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
+    assign la_data_out = {{(63-3){1'b0}}, sum};
 
-    counter #(
-        .BITS(BITS)
-    ) counter(
+    // Assuming LA probes [63:32] are for controlling the count register  
+    assign la_write = ~la_oenb[63:32];
+
+    // Assuming LA probes [65:64] are for controlling the count clk & reset  
+    assign clk = wb_clk_i;
+    assign rst = wb_rst_i;
+    // Assuming LA probes [36] controls the wena;
+    assign wena = la_data_in[36];
+
+    // Assign the IO output to observe the status of BEC's FSM 
+    assign io_out = status;
+
+    always @(posedge clk) begin
+        if (rst) begin
+            reg_num1 <= 0;
+            reg_num2 <= 0;
+        end
+        else begin
+            if (|la_write && ~wena)  begin
+                case (la_data_in[35])
+                    1'b0: begin
+                        reg_num1 <= la_data_in[34:32];
+                    end
+                    1'b1:begin
+                        reg_num2 <= la_data_in[34:32];
+                    end
+                endcase;
+            end       
+        end
+    end
+
+
+
+
+    add #(
+    ) add(
         .clk(clk),
         .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i[BITS-1:0]),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:64-BITS]),
-        .count(count)
+        .num1(reg_num1),
+        .num2(reg_num2),
+        // .la_write(la_write),
+        // .la_input(la_data_in[36:32]),
+        .status(status),
+        .wena(wena),
+        .done(done),
+        .sum(sum)
     );
 
 endmodule
 
-module counter #(
-    parameter BITS = 16
-)(
+module add(
     input clk,
     input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output reg ready,
-    output reg [BITS-1:0] rdata,
-    output reg [BITS-1:0] count
+    // input [31:0] la_write,
+    input [2:0] num1,
+    input [2:0] num2,
+    // input [4:0] la_input,
+    input wena,
+    output [2:0] status,
+    output done,
+    output [2:0] sum
+    
 );
+reg [2:0] sum;
+reg done;
+reg data_ready;
+reg [2:0] count;
+reg busy;
+parameter IDLE = 2'b00, READY = 2'b01, BUSY = 2'b10, DONE = 2'b11;
+// assign busy = (current_state == BUSY) ? 1 : 0;
 
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 1'b0;
-            ready <= 1'b0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1'b1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
+reg current_state;
+wire next_state;
+assign status = data_ready << 2 | busy << 1 | done ;
+
+always @(posedge clk )
+    begin
+        if(reset)
+            current_state <= IDLE;
+        else 
+            current_state <= next_state;
     end
 
+always@(wena, done) begin
+    case(current_state)
+        IDLE: begin
+            if (wena) begin
+                next_state <= READY;
+            end else begin
+                next_state <= IDLE;
+            end
+        end
+
+        READY: begin
+            next_state <= BUSY;
+        end
+
+        BUSY: begin
+            if (done) begin
+                next_state <= DONE;
+            end else begin
+                next_state <= BUSY;
+            end
+        end
+
+        DONE: begin
+            next_state <= IDLE;
+        end
+    endcase
+end
+
+
+always @(posedge clk) begin
+    if (reset) begin
+        done        <= 1'b0;
+        busy        <= 1'b0;
+        data_ready  <= 1'b0;
+        count       <= 3'b0;
+    end
+    else begin
+        case (current_state)
+            IDLE: begin
+                done        <= 1'b0;
+                busy        <= 1'b0;
+                data_ready  <= 1'b0;
+                count       <= 3'b0;
+            end    
+
+            READY: begin
+                data_ready <= 1'b1;
+            end
+
+            BUSY: begin
+                busy <= 1'b1;
+                data_ready <= 1'b0;
+
+                count <= count + 1; 
+                if (count == 4) begin
+                    done <= 1'b1;
+                end else begin
+                    done <= 1'b0;
+                end
+            end
+
+            DONE: begin
+                sum <= num1 + num2;
+            end
+        endcase   
+    end
+end
 endmodule
 `default_nettype wire
