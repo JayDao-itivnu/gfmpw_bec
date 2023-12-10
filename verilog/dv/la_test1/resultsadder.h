932fd4e001a5ffcfec1cfc488cc081e945bb1e93
Manh-Hiep Dao <1907.hiepmanhdao@gmail.com>
2023-12-10 09:53:23 +0700

    Add the la_test1 (template for adder, not fixed); please modify for being compatible with new version 23.12.10

---
+++ verilog/dv/la_test1/resultsadder.h
@@ -0,0 +1,10 @@
+#ifndef HEXADECIMAL_ARRAY_H
+#define HEXADECIMAL_ARRAY_H
+
+unsigned char a_array[] = {0x5, 0x6};
+
+unsigned char b_array[] = {0x1, 0x7};
+
+unsigned char c_array[] = {0x6, 0x5};
+
+#endif /* INTEGER_ARRAY_H */
